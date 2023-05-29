f3331f4570ff147d1f6db4658642d1758a26c019
Fernando Cuellar <fercuellar2010@hotmail.com>
2023-05-28 18:00:16 -0600

    doxy ino

--- Arduino/extra/extra.ino
+++ Arduino/extra/extra.ino
@@ -1,21 +1,44 @@
-// Definir la velocidad de comunicación serial
+/**
+ * @file
+ * @brief Código de Arduino para la comunicación serial
+ */
+
+/**
+ * @def BAUD_RATE
+ * @brief Velocidad de comunicación serial
+ */
 #define BAUD_RATE 9600
 
-// Variable para almacenar los datos recibidos
+/**
+ * @var receivedData
+ * @brief Variable para almacenar los datos recibidos
+ */
 String receivedData;
 
+/**
+ * @brief Configuración inicial del Arduino
+ *
+ * Inicializa la comunicación serial y espera hasta que se establezca la conexión.
+ */
 void setup() {
   // Inicializar la comunicación serial
   Serial.begin(BAUD_RATE);
+  
   while (!Serial) {
     // Esperar a que se establezca la comunicación serial
   }
 }
 
+/**
+ * @brief Función principal de Arduino
+ *
+ * Lee los datos recibidos por la comunicación serial y envía una respuesta a Python.
+ */
 void loop() {
   if (Serial.available()) {
     // Leer los datos recibidos hasta que se alcance el final del mensaje
     char receivedChar = Serial.read();
+    
     if (receivedChar != '\n') {
       receivedData += receivedChar;
     } else {
